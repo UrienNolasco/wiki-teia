81dd566e150bfd82b3f24cfbdfa3c40cbd85b084
UrienNolasco <urien.dragon@gmail.com>
2025-03-25 09:59:11 -0300

    feat: finalizada tela de login

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 const About = () => {
   return (
@@ -6,14 +6,19 @@ const About = () => {
       <div className=" w-full flex h-full">
         {/* Lado esquerdo azul */}
         <div className="w-1/2 bg-blue-500 min-h-screen">
-          <Image src={"computerteia.svg"} alt="Imagem de computador" width={1000} height={1000} />
+          <Image
+            src={"computerteia.svg"}
+            alt="Imagem de computador"
+            width={1000}
+            height={1000}
+          />
         </div>
         {/* Lado direito com texto */}
         <div className="w-1/2 flex flex-col justify-center text-center p-12">
-          <h2 className="text-4xl font-bold text-center uppercase ">
+          <h2 className="text-4xl font-bold  uppercase text-left">
             Sobre o nosso <br></br> aplicativo
           </h2>
-          <p className="mt-4 text-lg text-gray-700">
+          <p className="mt-4 text-lg text-gray-700 text-justify">
             O <span className="font-bold">WIKITEIA</span> é uma plataforma web
             que conecta aprendizado e prática, proporcionando um ambiente
             dinâmico para estudos e desenvolvimento acadêmico. Nosso objetivo é
